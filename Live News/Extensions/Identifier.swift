b9665989277eb6a67c9267dd3959aca0dabd4626
Husseinkandil <80462205+husseinkandil@users.noreply.github.com>
2022-02-24 22:07:11 +0200

    Table view cell design (#2)
    * - cell designed
    - content view adde
    
    * - bug fixes on sportNews controller
    - creating news protocol
    
    Co-authored-by: zeinAbdallah <93872238+zeinAbdallah@users.noreply.github.com>

--- Live News/Extensions/Identifier.swift
+++ Live News/Extensions/Identifier.swift
@@ -11,6 +11,46 @@ extension UIView {
     static var identifier: String {
         return String(describing: self)
     }
+    @IBInspectable
+    public var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    public var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+             layer.shadowOpacity = newValue
+        }
+    }
+
+    @IBInspectable
+    public var shadowOffset: CGSize {
+        get {
+            layer.shadowOffset
+        }
+        set {
+             layer.shadowOffset = newValue
+        }
+    }
+
+    @IBInspectable
+    public var shadowColor: UIColor {
+        get {
+            return UIColor(cgColor: layer.shadowColor ?? UIColor.clear.cgColor)
+        }
+
+        set {
+            layer.shadowColor = newValue.cgColor
+        }
+    }
 }
 
 extension UIImage {
--- Live News/News API Requst/NewsData.swift
+++ Live News/News API Requst/NewsData.swift
@@ -7,18 +7,50 @@
 
 import UIKit
 
+protocol News {
+    var imageURL: String? { get }
+    var author: String? { get }
+    var title: String? { get }
+    var description: String? { get }
+    var url: String? { get }
+}
+
 struct NewsData: Codable {
     let author: String?
     let title: String?
     let description: String?
     let urlToImage: String?
     let url: String?
+//    let image: String?
 }
 
 struct NewsResponse: Codable {
     var articles: [NewsData]
 }
 
+struct SportNewsResponse: Codable {
+    var data: [SportData]
+}
+
+struct SportData: Codable {
+    let author: String?
+    let title: String?
+    let description: String?
+    let url: String?
+    let image: String?
+}
+
+extension NewsData: News {
+    var imageURL: String? {
+        urlToImage
+    }
+}
+
+extension SportData: News {
+    var imageURL: String? {
+        image
+    }
+}
 
 //"author"
 //"title"
